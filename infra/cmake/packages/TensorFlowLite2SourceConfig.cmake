f43ef119020e6dd33583bc5f8de5382a39db62e1
Pavel Kuksov <p.kuksov@partner.samsung.com>
2020-05-13 15:26:34 +0300

    [cmake] Infrastructure for loading and build TF Lite2 with delegates
    This PR adds infrastructure for downloading and build TensorFlow Lite 2
    with GPU and Hexagon delegates (on aarch64.android platfrom)
    
    Signed-off-by: Pavel Kuksov <p.kuksov@partner.samsung.com>

---
+++ infra/cmake/packages/TensorFlowLite2SourceConfig.cmake
@@ -0,0 +1,18 @@
+function(_TensorFlowLite2Source_import platform)
+    if(NOT DOWNLOAD_TENSORFLOW_LITE2)
+        set(TensorFlowLite2Source_FOUND FALSE PARENT_SCOPE)
+        return()
+    endif(NOT DOWNLOAD_TENSORFLOW_LITE2)
+
+    nnas_include(ExternalSourceTools)
+    nnas_include(OptionTools)
+
+    envoption(TENSORFLOW_LITE2_URL https://github.com/tensorflow/tensorflow/archive/v2.2.0-rc0.tar.gz)
+    ExternalSource_Get("tensorflow2-${platform}" ${DOWNLOAD_TENSORFLOW_LITE2} ${TENSORFLOW_LITE2_URL})
+
+
+    set(TensorFlowLite2Source_DIR ${tensorflow2-${platform}_SOURCE_DIR} PARENT_SCOPE)
+    set(TensorFlowLite2Source_FOUND ${tensorflow2-${platform}_SOURCE_GET} PARENT_SCOPE)
+endfunction(_TensorFlowLite2Source_import)
+
+_TensorFlowLite2Source_import(${platform})
